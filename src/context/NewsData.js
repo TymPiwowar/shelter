1ff52c3b24a09a6d45886bc7cd7bc3df80cf6fe9
TymPiwowar <tymoteusz.piwowarski@gmail.com>
2025-07-09 19:31:36 +0200

    news feed done

---
+++ src/context/NewsData.js
@@ -0,0 +1,61 @@
+import news1Image from '../assets/img/dogFood.jpg'
+import news2Image from '../assets/img/dogHouse.jpg'
+import news3Image from '../assets/img/dogOnLeash.jpg'
+import news4Image from '../assets/img/dogTraining.jpg'
+import news5Image from '../assets/img/catBirthday.png'
+import news6Image from '../assets/img/dogRex.jpg'
+import news7Image from '../assets/img/surgeryCat.jpg'
+import news8Image from '../assets/img/cleaning.jpg'
+
+const newsData = [
+	{
+		newsId: 1,
+		title: 'Nowa dostawa karmy!',
+		text: 'Dzięki dotacjom wolontariuszy udało się nam zakupić 100 kilogramów karmy najwyższej jakości.',
+		image: news1Image,
+	},
+	{
+		newsId: 2,
+		title: 'Nowe domki dla psów',
+		text: 'Zakupiliśmy już 12 nowych domków, które pomogą naszym przyjaciołom przetrwać zimę.',
+		image: news2Image,
+	},
+	{
+		newsId: 3,
+		title: 'Dzień otwarty w schronisku!',
+		text: 'Zapraszamy na poznanie naszych psich i kocich przyjaciół!',
+		image: news3Image,
+	},
+	{
+		newsId: 4,
+		title: 'Szukamy pracownika',
+		text: 'Poszukujemy osoby zajmującej się szkoleniem psów.',
+		image: news4Image,
+	},
+	{
+		newsId: 5,
+		title: 'Urodziny schroniska – świętujemy razem!',
+		text: 'Nasze schronisko kończy 10 lat! Z tej okazji zapraszamy na piknik rodzinny z atrakcjami, pokazami psich sztuczek i możliwością poznania naszych podopiecznych.',
+		image: news5Image,
+	},
+	{
+		newsId: 6,
+		title: ' Adopcja miesiąca: Poznaj Reksa!',
+		text: 'Reks to 3-letni energiczny piesek, który czeka na swój wymarzony dom. Uwielbia spacery i zabawę z piłką. Może to właśnie Ty dasz mu szansę na lepsze życie?',
+		image: news6Image,
+	},
+	{
+		newsId: 7,
+		title: 'Bezpłatna sterylizacja dla adopcyjnych zwierząt',
+		text: 'W ramach akcji promującej odpowiedzialną adopcję, oferujemy darmową sterylizację każdego zwierzęcia adoptowanego w lipcu.',
+		image: news7Image,
+	},
+	{
+		newsId: 7,
+		title: 'Wielkie czyszczenie boksów – szukamy wolontariuszy',
+		text: 'Potrzebujemy pomocnych rąk! W najbliższą sobotę organizujemy porządki w schronisku – dołącz do nas i pomóż stworzyć lepsze warunki zwierzakom.',
+		image: news8Image,
+	},
+]
+
+export default newsData
--- src/pages/Newsfeed/Newsfeed.jsx
+++ src/pages/Newsfeed/Newsfeed.jsx
@@ -1,25 +1,81 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Newsfeed.css'
+import { useNavigate } from 'react-router-dom'
+import newsData from '../../context/NewsData'
 
 export default function Newsfeed() {
+	const navigate = useNavigate()
+	const itemsPerPage = 6
+	const [currentPage, setCurrentPage] = useState(1)
+	const totalPages = Math.ceil(newsData.length / itemsPerPage)
+
+	const paginatedNews = newsData.slice((currentPage - 1) * itemsPerPage, currentPage * itemsPerPage)
+
+	const pageNumbers = Array.from({ length: totalPages }, (_, i) => i + 1)
+
 	return (
 		<>
 			<div className='NewsfeedContainer'>
 				<h1>Aktualności</h1>
 				<div className='newsGrid'>
-					<div className='newsCard'>News</div>
-					<div className='newsCard'>News</div>
-					<div className='newsCard'>News</div>
-					<div className='newsCard'>News</div>
-					<div className='newsCard'>News</div>
-					<div className='newsCard'>News</div>
-					<div className='newsCard'>News</div>
-					<div className='newsCard'>News</div>
-					<div className='newsCard'>News</div>
-					<div className='newsCard'>News</div>
-					<div className='newsCard'>News</div>
+					{paginatedNews.map(news => (
+						<div key={news.id} className='newsCard'>
+							<div className='newsImage' style={{ backgroundImage: `url(${news.image})` }}></div>
+							<div className='newsTitleContainer'>
+								<h2>{news.title}</h2>
+							</div>
+							<div className='newsDescriptionContainer'>
+								<p>{news.text}</p>
+							</div>
+							<button
+								className='knowMoreAboutNewsButton'
+								onClick={() => navigate(`/newsfeed/news/${encodeURIComponent(news.id)}`)}>
+								Czytaj więcej
+							</button>
+						</div>
+					))}
+				</div>
+
+				<div className='pagesButtonsContainer'>
+					<button disabled={currentPage === 1} onClick={() => setCurrentPage(p => p - 1)} className='extremeButton'>
+						‹ Poprzednia
+					</button>
+					{pageNumbers.map(pageNum => (
+						<button
+							key={pageNum}
+							onClick={() => setCurrentPage(pageNum)}
+							className={pageNum === currentPage ? 'active circleButton' : 'circleButton'}>
+							{pageNum}
+						</button>
+					))}
+					<button
+						disabled={currentPage === totalPages}
+						onClick={() => setCurrentPage(p => p + 1)}
+						className='extremeButton'>
+						Następna ›
+					</button>
 				</div>
 			</div>
+
+			{/* <div className='pagesButtonsContainer'>
+				<button disabled={currentPage === 1} onClick={() => setCurrentPage(p => p - 1)} className='extremeButton'>
+					‹ Poprzednia
+				</button>
+				{pageNumbers.map(pageNum => (
+					<button
+						key={pageNum}
+						onClick={() => setCurrentPage(pageNum)}
+						className={pageNum === currentPage ? 'active circleButton' : 'circleButton'}>
+						{pageNum}
+					</button>
+				))}
+				<button
+					disabled={currentPage === totalPages}
+					onClick={() => setCurrentPage(p => p + 1)}
+					className='extremeButton'>
+					Następna ›
+				</button>
+			</div> */}
 		</>
 	)
 }
